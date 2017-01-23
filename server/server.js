0c5bc6f6e9ea17c742bdec109346b3214c00c506
Guilherme <gui.mochila@gmail.com>
2017-01-23 19:30:44 +0000

    Fixed port variable

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const publicPath = path.join(__dirname, '../public');
 app.use(express.static(publicPath));
 
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is up on port 3000');
 });
 
