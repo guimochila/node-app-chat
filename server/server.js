b0c8d90553d5e0b5d648736c4b105814fb776f2e
Guilherme <gui.mochila@gmail.com>
2017-01-27 22:16:42 +0000

    Send messages to only people in same room

--- server/server.js
+++ server/server.js
@@ -18,8 +18,6 @@ const users = new Users();
 app.use(express.static(publicPath));
 
 io.on('connection', (socket) => {
-  console.log('New user connected');
-
   socket.on('join', (params, callback) => {
     if(!isRealString(params.name) || !isRealString(params.room)) {
       return callback('Name and room name are required');
@@ -37,13 +35,24 @@ io.on('connection', (socket) => {
   });
 
   socket.on('createMessage', (message, callback) => {
-    console.log(message);
-    io.emit('newMessage', generateMessage(message.from, message.text));
-    callback();
+    var user = users.getUser(socket.id);
+
+    if (user && isRealString(message.text)) {
+      io.to(user.room).emit('newMessage', generateMessage(user.name, message.text));
+   
+    }
+
+   callback();
   });
 
   socket.on('createLocationMessage', (coords) => {
-    io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    var user = users.getUser(socket.id);
+
+    if (user) {
+      io.to(user.room).emit('newLocationMessage', generateLocationMessage(user.name, coords.latitude, coords.longitude));
+   
+    }
+    // io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
   });
 
   socket.on('disconnect', () => {
